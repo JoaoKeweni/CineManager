102e1d8f8646042ddc4b362dfff657488ab15e23
Joaokeweni <joaokeweni49@gmail.com>
2026-03-18 10:54:04 -0300

    Reforçando sintaxe

--- Catalogo/src/Condicional.java
+++ Catalogo/src/Condicional.java
@@ -1,2 +1,18 @@
 public class Condicional {
+    public static void main(String[] args) {
+        int anoDeLancamento = 2021;
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.1;
+        String tipoPlano = "plus";
+
+        if (anoDeLancamento >= 2022){
+            System.out.println("Lançamentos que os clientes estao curtindo");
+        } else {
+            System.out.println("Filmes ruins");
+        }
+
+        if (incluidoNoPlano || tipoPlano.equals("plus")){
+            System.out.println("está ok");
+        }
+    }
 }
