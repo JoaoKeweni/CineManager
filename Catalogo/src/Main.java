b749ac6cbf3c1519699d7f748451dd0511c2901c
Joaokeweni <joaokeweni49@gmail.com>
2026-03-18 10:47:38 -0300

    Alguns aprendizados que precisavam de reforço

--- Catalogo/src/Main.java
+++ Catalogo/src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("olá mundo");
+        System.out.println("Bem vindo ao Screen Match");
+        System.out.println("Filme: Top Gun: Maverick");
+
+        int anoDeLancamento = 2022;
+        System.out.println("Ano de Lançamento " + anoDeLancamento);
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.0;
+
+        double media = (9.8 + 6.3 + 8.0) /3;
+        System.out.println(media);
+
+        String sinopse = "dasr";
+        System.out.println(sinopse);
+
     }
 }
