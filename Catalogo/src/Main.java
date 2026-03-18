c81f617b64f7794562d92d3bc8e585015a0af833
João Keweni S R <joaokeweni49@gmail.com>
2026-03-18 17:42:31 -0300

    reforco em herança

--- Catalogo/src/Main.java
+++ Catalogo/src/Main.java
@@ -1,5 +1,16 @@
+import br.com.alura.cinemanager.modelos.Filme;
+
 public class Main {
     public static void main(String[] args) {
-        Filme meuFilme = new Filme();
+        Filme meuFilme = new Filme ();
+        meuFilme.setNome("poderoso Chefão");
+        meuFilme.setAnoDeLancamento(1970);
+
+        meuFilme.exibeFichaTecnicna();
+        meuFilme.avalia(5);
+        meuFilme.avalia(3);
+        meuFilme.avalia(10);
+        System.out.println(meuFilme.pegaMedia());
+        System.out.println(meuFilme.getTotalDeAvaliacoes());
     }
 }
---
+++ Catalogo/src/br/com/alura/cinemanager/modelos/Serie.java
@@ -0,0 +1,40 @@
+package br.com.alura.cinemanager.modelos;
+
+public class Serie extends Titulo{
+    private int episodios;
+    private int temporadas;
+    private boolean ativa;
+    private int minutosPorEpisodio;
+
+    public int getEpisodios() {
+        return episodios;
+    }
+
+    public void setEpisodios(int episodios) {
+        this.episodios = episodios;
+    }
+
+    public int getTemporadas() {
+        return temporadas;
+    }
+
+    public void setTemporadas(int temporadas) {
+        this.temporadas = temporadas;
+    }
+
+    public boolean isAtiva() {
+        return ativa;
+    }
+
+    public void setAtiva(boolean ativa) {
+        this.ativa = ativa;
+    }
+
+    public int getMinutosPorEpisodio() {
+        return minutosPorEpisodio;
+    }
+
+    public void setMinutosPorEpisodio(int minutosPorEpisodio) {
+        this.minutosPorEpisodio = minutosPorEpisodio;
+    }
+}
