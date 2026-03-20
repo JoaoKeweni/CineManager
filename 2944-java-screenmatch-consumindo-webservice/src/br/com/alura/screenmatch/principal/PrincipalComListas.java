71a3cf222548cdd1367ef87f54ea241162e5fc4e
Joaokeweni <joaokeweni49@gmail.com>
2026-03-20 10:08:42 -0300

    aplicando o framework Gson

--- 2944-java-screenmatch-consumindo-webservice/src/br/com/alura/screenmatch/principal/PrincipalComListas.java
+++ 2944-java-screenmatch-consumindo-webservice/src/br/com/alura/screenmatch/principal/PrincipalComListas.java
@@ -3,51 +3,37 @@ package br.com.alura.screenmatch.principal;
 import br.com.alura.screenmatch.modelos.Filme;
 import br.com.alura.screenmatch.modelos.Serie;
 import br.com.alura.screenmatch.modelos.Titulo;
+import com.google.gson.Gson;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.LinkedList;
-import java.util.List;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.*;
 
 public class PrincipalComListas {
-    public static void main(String[] args) {
-        Filme meuFilme = new Filme("O poderoso chefão", 1970);
-        meuFilme.avalia(9);
-        Filme outroFilme = new Filme("Avatar", 2023);
-        outroFilme.avalia(6);
-        var filmeDoPaulo = new Filme("Dogville", 2003);
-        filmeDoPaulo.avalia(10);
-        Serie lost = new Serie("Lost", 2000);
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("Digite o filme: ");
+        var filme = leitor.nextLine();
 
-        Filme f1 = filmeDoPaulo;
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://www.omdbapi.com/?t="+ filme + "&apikey=e3aa7a"))
+                .build();
 
-        ArrayList<Titulo> lista = new ArrayList<>();
-        lista.add(filmeDoPaulo);
-        lista.add(meuFilme);
-        lista.add(outroFilme);
-        lista.add(lost);
-        for (Titulo item: lista) {
-            System.out.println(item.getNome());
-            if (item instanceof  Filme filme && filme.getClassificacao() > 2) {
-                System.out.println("Classificação " + filme.getClassificacao());
-            }
-        }
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
 
-        List<String> buscaPorArtista = new LinkedList<>();
-        buscaPorArtista.add("Adam Sandler");
-        buscaPorArtista.add("Paulo");
-        buscaPorArtista.add("Jacqueline");
-        System.out.println(buscaPorArtista);
 
-        Collections.sort(buscaPorArtista);
-        System.out.println("Depois da ordenação");
-        System.out.println(buscaPorArtista);
-        System.out.println("Lista de titulos ordenados");
-        Collections.sort(lista);
-        System.out.println(lista);
-        lista.sort(Comparator.comparing(Titulo::getAnoDeLancamento));
-        System.out.println("Ordenando por ano");
-        System.out.println(lista);
+        String json = response.body();
+        System.out.println(json);
+
+        Gson gson = new Gson();
+
+        Titulo meuTitulo = gson.fromJson(json, Titulo.class);
+        System.out.println(meuTitulo);
     }
 }
